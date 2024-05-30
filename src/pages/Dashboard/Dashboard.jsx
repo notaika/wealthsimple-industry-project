4bac9c05b83c23da56c2a1c47c57b48da2c85bb2
notaika <ercamanalo@gmail.com>
2024-05-30 15:55:10 -0700

    feat: added 'Learn and Earn' component for our main idea.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import wealthProto from '../../assets/wealthsimple-proto.png';
+import rightArrow from '../../assets/right-arrow.png';
 import './Dashboard.scss';
 
 export default function Dashboard() {
   return (
     <div className="dashboard">
         <div className="dashboard__resources">
-            
+            <div className="dashboard__resources-card">
+                <h2 className="dashboard__resources-title">Learn and Earn</h2>
+                <Link to='/resources' className='dashboard__resources-link'>Explore Resources <img src={rightArrow} alt="A right arrow icon for link" className="dashboard__resources-icon" /></Link>
+            </div>
         </div>
     </div>
   )
