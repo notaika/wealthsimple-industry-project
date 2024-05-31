b3d71f792152ad0712222d19c73286f6e61b849d
notaika <ercamanalo@gmail.com>
2024-05-31 10:02:06 -0700

    feat: fixed components to fit in together. populated resources page list more. edited README.md styling fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,14 +9,16 @@ export default function Header() {
     const isActiveCurriculum = location.pathname.includes('curriculum')
     const isActiveQuiz = location.pathname.includes('quiz');
 
+    const isCourseActive = location.pathname.includes('quiz') || location.pathname.includes('curriculum');
+
   return (
     <header className='head'>
         <div className="head--top">
             <Link to='/'><img src={wsLogo} alt="Wealthsimple logo" className="head__logo" /></Link>
             <Link to='/rewards' className="head__credits">Credits: 40</Link>
         </div>
-        <div className="head--bot">
-        <nav className="head__nav">
+        <div className={`head--bot ${isCourseActive ? '' : 'head--active'}`}>
+        <nav className="head__nav ">
             <Link to='/resources/curriculum' className={`head__link ${isActiveCurriculum ? 'head__link--active' : ''}`}>Curriculum</Link>
             <Link to='/resources/quiz' className={`head__link ${isActiveQuiz ? 'head__link--active' : ''}`}>Quiz</Link>
         </nav>
