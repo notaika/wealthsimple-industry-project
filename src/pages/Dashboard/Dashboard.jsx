96235f80786848a522be1ae01b6f304d455a992d
Brittany Barnard <brittanyabarnard@gmail.com>
2024-05-30 20:37:02 -0400

    styling dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import wealthProto from '../../assets/wealthsimple-proto.png';
 import rightArrow from '../../assets/right-arrow.png';
+import vault from '../../assets/wealthsimple-learn-and-earn-image.png';
 import './Dashboard.scss';
 
 export default function Dashboard() {
@@ -10,6 +11,7 @@ export default function Dashboard() {
         <div className="dashboard__resources">
             <div className="dashboard__resources-card">
                 <h2 className="dashboard__resources-title">Learn and Earn</h2>
+                <img className='dashboard__resources-image' src={vault} alt='vault' />
                 <Link to='/resources' className='dashboard__resources-link'>Explore Resources <img src={rightArrow} alt="A right arrow icon for link" className="dashboard__resources-icon" /></Link>
             </div>
         </div>
